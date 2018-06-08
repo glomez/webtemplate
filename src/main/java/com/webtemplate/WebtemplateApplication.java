0b283ca61ae8883c7a687a7aba10fc18ebd00026
glom ez <kouatchdevopps@gmail.com>
2018-06-08 07:41:59 +0100

    master webtemplate

---
+++ src/main/java/com/webtemplate/WebtemplateApplication.java
@@ -0,0 +1,12 @@
+package com.webtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtemplateApplication.class, args);
+	}
+}
